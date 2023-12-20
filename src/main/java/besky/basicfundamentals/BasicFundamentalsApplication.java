ac27f2c1473d806909f3993b11ac9cae1550e925
besky <onlybesky@gmail.com>
2023-12-20 22:33:14 +0900

    Spring initial setting

---
+++ src/main/java/besky/basicfundamentals/BasicFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package besky.basicfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/besky/basicfundamentals/BasicFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package besky.basicfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
